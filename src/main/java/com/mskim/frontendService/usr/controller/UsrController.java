152cb0f1e057bbfbba7027ddac3565c296fc75ca
minsu kim <779003@naver.com>
2018-04-10 19:42:32 +0900

    각 메도스마다 처리 메시지 화면에 출력하도록 추가, 메소드 추가(registerUsr, idCheck, findPasswordPage, findPassword, findPasswordGetAnswer, modifyUsrPage, modifyUsr ) 및 코드 정리

--- src/main/java/com/mskim/frontendService/usr/controller/UsrController.java
+++ src/main/java/com/mskim/frontendService/usr/controller/UsrController.java
@@ -1,6 +1,5 @@
 package com.mskim.frontendService.usr.controller;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +7,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mskim.backendservice.member.service.MemberService;
 import com.mskim.backendservice.member.vo.MemberVo;
@@ -24,7 +25,7 @@ public class UsrController {
 	MemberService memberService;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
-	public String goLogin(HttpServletRequest request, HttpSession session, String currUrl) {
+	public String goLogin(HttpSession session, String currUrl) {
 
 		MemberVo loginMember = (MemberVo) session.getAttribute("loginSession");
 
@@ -35,17 +36,9 @@ public class UsrController {
 			return "redirect:" + currUrl;
 		}
 	}
-		
 	
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String memberLogin(MemberVo memberVo, HttpSession session, String currUrl) {
-
-		//임시 로그인처리
-		memberVo.setId("admin");
-		memberVo.setPassword("alstn12#");
-		/////////////////////
-		
-		memberVo.setPassword(Integer.toString(memberVo.getPassword().hashCode()));
+	public String memberLogin(MemberVo memberVo, HttpSession session, String currUrl, RedirectAttributes redirectAttr) {
 		
 		if (loginService.memberLogin(memberVo) > 0) {
 			session.setAttribute("loginSession", memberService.selectMember(memberVo.getId()));
@@ -53,25 +46,103 @@ public class UsrController {
 			currUrl = (currUrl == null || currUrl == "") ? "/docs" : currUrl;
 			return "redirect:" + currUrl;
 		} else {
-			return "usr/login";
+			redirectAttr.addFlashAttribute("alert","비밀번호가 틀렸습니다.");
+			redirectAttr.addFlashAttribute("memberId",memberVo.getId());
+			return "redirect:/usr/login";
 		}
-
 	}
-	
 	
 	@RequestMapping(value="/logout")
 	public String logout(Model model, HttpSession session){
 		session.removeAttribute("loginSession");
-		
 		return "redirect:/docs";
 	}
 
 	@RequestMapping(value="/register", method = RequestMethod.GET)
-	public String register(Model model, HttpSession session){
+	public String registerUsrPage(Model model, HttpSession session){
 		session.removeAttribute("loginSession");
-		
 		return "usr/registerUsr";
 	}
+
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String registerUsr(Model model, MemberVo memberVo, RedirectAttributes redirectAttr){
+		
+		if(!memberService.alreadyHasValue(memberVo.getId())) {
+			memberService.insertMember(memberVo);
+			redirectAttr.addFlashAttribute("alert", "회원가입이 완료되었습니다.");
+			return "redirect:/docs";
+		}else {
+			redirectAttr.addFlashAttribute("alert", "잘못된 접근입니다.");
+			return "redirect:/docs";
+		}
+	}
 	
+	@RequestMapping(value="/validCheck/id", method = RequestMethod.GET)
+	public @ResponseBody boolean idCheck(String memberId) {
+		return memberService.alreadyHasValue(memberId);
+	}
+
+	@RequestMapping(value="/findPassword", method = RequestMethod.GET)
+	public String findPasswordPage(Model model, HttpSession session){
+			return "usr/findPassword";
+	}
 	
+	@RequestMapping(value="/question", method = RequestMethod.POST)
+	public String findPassword(Model model, HttpSession session, String memberId, RedirectAttributes redirectAttr){
+		
+		if(memberService.alreadyHasValue(memberId)) {
+			MemberVo memberVo = memberService.selectMember(memberId);
+			model.addAttribute("question", memberVo.getQuestion());
+			model.addAttribute("memberId", memberId);
+			return "/usr/question";
+		}else {
+			redirectAttr.addFlashAttribute("alert", "없는 계정입니다.");
+			return "redirect:/usr/findPassword";
+		}
+	}
+
+	@RequestMapping(value="/answer", method = RequestMethod.POST)
+	public String findPasswordGetAnswer(Model model, MemberVo memberVo, RedirectAttributes redirectAttr){
+		
+		if(memberService.questionAndAnswer(memberVo)) {
+			memberService.resetPassword(memberVo);
+			redirectAttr.addFlashAttribute("alert", "비밀번호가 초기화되었습니다.\\n비밀번호가 아이디와 같게 변경되었으니 로그인 후 마이페이지에서 수정해주세요.");
+			return "redirect:/docs";
+		}else {
+			model.addAttribute("alert", "틀렸습니다. 다시 입력해주세요.");
+			model.addAttribute("question", memberVo.getQuestion());
+			model.addAttribute("memberId", memberVo.getId());
+			return "/usr/question";
+		}
+	}
+	
+	@RequestMapping(value ="modify", method = RequestMethod.GET)
+	public String modifyUsrPage(Model model, HttpSession session) {
+		        
+        if(session == null) {
+        	return "redirect:/usr/login?currUrl=/usr/modify";
+        }
+        if(loginService.getLoginMember(session) == null){
+        	return "redirect:/usr/login?currUrl=/usr/modify";
+        }
+                
+        MemberVo memberVo = loginService.getLoginMember(session);
+        model.addAttribute("memberVo", memberVo);
+        
+		return "/usr/modifyUsr";
+	}
+	
+	@RequestMapping(value ="modify", method = RequestMethod.POST)
+	public String modifyUsr(Model model, HttpSession session, MemberVo memberVo, RedirectAttributes redirectAttr) {
+		
+		MemberVo sessionMemberVo = loginService.getLoginMember(session);
+		if(!memberVo.getId().equals(sessionMemberVo.getId())) {
+			redirectAttr.addFlashAttribute("alert", "잘못된 접근입니다.");
+		}
+		memberService.updateMember(memberVo);
+		redirectAttr.addFlashAttribute("alert", "정보 수정이 완료되었습니다.");
+		return "redirect:/docs";
+	}
+
 }
+

3f30b91f59a6d15eef3eafabfd5b62da62c26f34
minsu kim <779003@naver.com>
2018-04-19 13:58:58 +0900

    apiseq, apiKey, requestUrl 널 체크, insertLog(DB저장) 위치 변경

--- src/main/java/com/mskim/backendservice/common/errorpage/interceptor/ErrorPageInterceptor.java
+++ src/main/java/com/mskim/backendservice/common/errorpage/interceptor/ErrorPageInterceptor.java
@@ -33,8 +33,7 @@ public class ErrorPageInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		
-		
+
 		long startTime = (Long) request.getAttribute("startTime");
 		long endTime = System.currentTimeMillis();
 		long takeTime = endTime - startTime;
@@ -43,41 +42,38 @@ public class ErrorPageInterceptor extends HandlerInterceptorAdapter{
 		String[] urlSplit = request.getRequestURI().split("/");
 		String errorCode = urlSplit[2];
 		String requestUrl = request.getParameter("url");
-		
-		logger.info("[returned ErrorPage] ===================================================");
-		
-		if(apiSeq != null) {
+		apiSeq = apiSeq == null ? "UNKNOWN" : apiSeq;
+		apiKey = apiKey == null ? "UNKNOWN" : apiKey;
+		requestUrl = requestUrl == null ? "UNKNOWN" : requestUrl;
+		LogVo logInfo;
+
+		logger.info("[Returned ErrorPage] ===================================================");
+
+		if (apiSeq != null) {
 			logger.info("| API Seq : " + apiSeq);
 		}
-		
-		if(errorCode != null) {
+
+		if (errorCode != null) {
 			logger.info("| Error code : " + errorCode);
-						
 		}
-		
-		if(request.getRemoteHost() != null) {
+
+		if (request.getRemoteHost() != null) {
 			logger.info("| getRemoteHost : " + request.getRemoteHost());
 		}
-		
-		if(apiKey != null) {
-			LogVo logInfo = new LogVo(apiKey, apiSeq, "FAILURE", errorCode, "API_USE");
-			String useInfo = "| API KEY : " + logInfo.getApi_key();
-			useInfo += ", Success? : " + logInfo.getSuccess();
-			useInfo += ", Error Code : " + logInfo.getError_code();
-			
-			logger.info(useInfo);
-			
-			try {
-				logService.insertLog(request, logInfo, requestUrl);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			
-		}else {
-			logger.info("| log info is null");
+
+		logInfo = new LogVo(apiKey, apiSeq, "FAILURE", errorCode, "RETURN_ERROR");
+		String useInfo = "| API KEY : " + logInfo.getApi_key();
+		useInfo += ", Success? : " + logInfo.getSuccess();
+		useInfo += ", Error Code : " + logInfo.getError_code();
+
+		logger.info(useInfo);
+
+		try {
+			logService.insertLog(request, logInfo, requestUrl);
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		
-	logger.info("L take Time : " + takeTime + " ms ==============================================");
-		
+
+		logger.info("L take Time : " + takeTime + " ms ==============================================");
 	}
 }
